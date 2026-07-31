05dfa83d7e2ebd5fede4e967e8cf67842f95be4b
Joshua Chui <Joshuachui03@gmail.com>
2026-07-30 21:52:09 -0600

    make delivery options interactive

--- data/cart.js
+++ data/cart.js
@@ -72,3 +72,17 @@ export function updateQuantity (productId, newQuantity) {
 
   saveToStorage();
 }
+
+export function updateDeliverOption(productId, deliveryOptionId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId === productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
+
+  saveToStorage();
+}
