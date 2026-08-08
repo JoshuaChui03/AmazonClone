a5f0fa6ced147928fa911a42eb1c80cb458895b2
Joshua Chui <Joshuachui03@gmail.com>
2026-08-07 22:37:41 -0600

    create automated tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,16 +1,22 @@
-export let cart = JSON.parse(localStorage.getItem("cart"));
+export let cart;
 
-if (!cart) {
-  cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-    deliveryOptionId: '1'
-  },
-    {
-      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-      quantity: 1,
-      deliveryOptionId: '2'
-    }];
+loadFromStorage();
+
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem("cart"));
+
+  if (!cart) {
+    cart = [{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+      deliveryOptionId: '1'
+    },
+      {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1,
+        deliveryOptionId: '2'
+      }];
+  }
 }
 
 function saveToStorage(){
@@ -63,7 +69,7 @@ export function calculateCartQuantity() {
   return cartQuantity;
 }
 
-export function updateQuantity (productId, newQuantity) {
+export function  updateQuantity (productId, newQuantity) {
   cart.forEach((cartItem) => {
     if (cartItem.productId === productId) {
       cartItem.quantity = newQuantity;
