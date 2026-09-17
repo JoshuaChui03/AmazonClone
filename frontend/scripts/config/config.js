c6eff7b7be6ba6bea52588167b142138194e19a9
Joshua Chui <Joshuachui03@gmail.com>
2026-09-17 01:24:25 -0600

    update API_URL to point to new production endpoint

--- frontend/scripts/config/config.js
+++ frontend/scripts/config/config.js
@@ -1,4 +1,4 @@
 export const API_URL =
     window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
         ? 'http://localhost:3500'
-        : 'https://amazon-clone-api-1729beffe502.herokuapp.com';
+        : 'http://api.amazonclone.joshuachui.net';
